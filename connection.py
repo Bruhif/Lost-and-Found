62eff6fcfbe3779f79085768f39d995f628f967b
Teentin <78711334+Bruhif@users.noreply.github.com>
2026-08-18 11:26:31 +0700

    Further security modifies

--- connection.py
+++ connection.py
@@ -25,12 +25,17 @@ app = Flask(__name__)
 CORS(app)
 
 DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+
 
 DB_CONFIG = {
-    "host": "100.121.226.108",
-    "port": 5432,
-    "database": "lnf",
-    "user": "teentin",
+    "host": DB_HOST,
+    "port": DB_PORT,
+    "database": DB_NAME,
+    "user": DB_USER,
     "password": DB_PASSWORD
 }
 
